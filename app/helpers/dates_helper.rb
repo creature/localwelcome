f5927ca33e67ae4c7fe42e53b60eddbadc3eadbb
Alex Pounds <alex+git@alexpounds.com>
2015-11-27 18:22:46 -0500

    Improve formatting of date stamps (throughout the app), and layout of date/time on specific events pages.

--- app/helpers/dates_helper.rb
+++ app/helpers/dates_helper.rb
@@ -1,9 +1,26 @@
 module DatesHelper
-  def long_friendly_date(date)
+
+  # Format a datestamp as "December 26, 2015 at 8:45 PM"
+  def long_friendly_datetime(date)
     date.strftime("%B %e, %Y at %l:%M%p")
   end
 
-  def short_friendly_date(date)
+  # Format a datestamp as "December 26, 2015"
+  def long_friendly_date(date)
+    date.strftime("%B %e, %Y")
+  end
+
+  # Format a datestamp as "Dec 26 - 20:45"
+  def short_friendly_datetime(date)
     date.strftime("%b %e - %k:%M")
+  end
+
+  # Format a datestamp as "Dec 26"
+  def short_friendly_date(date)
+    date.strftime("%b %e")
+  end
+
+  def friendly_time(date)
+    date.strftime("%l:%M%p")
   end
 end
