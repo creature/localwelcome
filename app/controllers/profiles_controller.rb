6ba0afdaa4b433873f02dda0abb801ffee2bc9a4
Alex Pounds <alex+git@alexpounds.com>
2015-12-02 18:15:16 -0500

    An edit page for the profiles controller, and a (broken) attempt at authorisation.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,8 +3,21 @@ class ProfilesController < ApplicationController
   end
 
   def edit
+    authorize! :edit, current_user
   end
 
   def update
+    authorize! :edit, current_user
+    if current_user.update_attributes(profile_params)
+      redirect_to profile_path(current_user), notice: "Profile updated!"
+    else
+      redirect_to :back, alert: "Sorry, your profile could not be updated."
+    end
+  end
+
+  protected
+
+  def profile_params
+    params.require(:user).permit(:name, :email, :telephone, :bio)
   end
 end
