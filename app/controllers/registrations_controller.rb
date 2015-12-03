8b6ea1f75c3add21a6665b93bc114f4c3cfff1d0
Alex Pounds <alex+git@alexpounds.com>
2015-12-03 17:16:16 -0500

    Automatically create a subscription when a user signs up from a chapter page, along with tests for it.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
+  # Show a "Sign up for Local Welcome" page.
+  def new
+    @chapter_id = params[:chapter]
+    super
+  end
+
+  # Create a new user, along with a subscription for a chapter if they came via a chapter page.
+  def create
+    super
+    if params[:chapter_id] && Chapter.exists?(params[:chapter_id])
+      resource.subscriptions.create(chapter_id: params[:chapter_id])
+    end
+  end
+
   protected
 
+  # Redirect the user to the subscriptions path after signup.
   def after_sign_up_path_for(resource)
     subscriptions_path
   end
---
+++ spec/features/signing_up_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+feature "Signing Up to Local Welcome" do
+  let(:chapter) { FactoryGirl.create(:chapter) }
+  let(:email) { Faker::Internet.safe_email }
+  let(:password) { Faker::Lorem.characters(10) }
+
+  scenario "Signing up works" do
+    visit new_user_registration_path
+    fill_in_registration_form
+
+    expect { click_button "Sign up" }.to change { User.count }.by 1
+  end
+
+  scenario "Signing up doesn't create a subscription automatically" do
+    visit new_user_registration_path
+    fill_in_registration_form
+
+    expect { click_button "Sign up"}.not_to change { Subscription.count }
+  end
+
+  context "Signing up from a chapter page" do
+    scenario "Signing up subscribes the user to a given mailing list" do
+      visit chapter_path(chapter)
+      click_link "Sign up to Local Welcome #{chapter.name}"
+      fill_in_registration_form
+
+      expect { click_button "Sign up" }.to change { Subscription.count }.by 1
+      subscription = Subscription.first
+      expect(subscription.user).to be
+      expect(subscription.chapter).to eq chapter
+    end
+
+    scenario "Signing up with an invalid chapter ID doesn't result in errors" do
+      visit new_user_registration_path(chapter: 4567)
+      fill_in_registration_form
+      expect { click_button "Sign up" }.not_to change { Subscription.count }
+      expect(User.where(email: email).exists?).to be true
+    end
+  end
+end
+
+def fill_in_registration_form
+  fill_in :user_email, with: email
+  fill_in :user_password, with: password
+  fill_in :user_password_confirmation, with: password
+end
+
