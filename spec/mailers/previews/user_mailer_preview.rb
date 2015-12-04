ed6fc03af1c56b85ac3f83629cba4e054db4233b
Alex Pounds <alex+git@alexpounds.com>
2015-12-04 12:25:08 -0500

    Make preview of invite mail work, and fix bug with user name display.

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,6 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/invite
   def invite
-    UserMailer.invite
+    invitation = FactoryGirl.create(:invitation)
+    UserMailer.invite(invitation)
   end
-
 end
