7ae2af9973e4121351858135f6a56b7284bf742c
Chickencoding <wardandsmoke@laposte.net>
2015-12-05 10:10:06 +0100

    Adds users, admin user and role to the seeds, along with stats at end of it.
    These seeds has added in db/development.sqlite.
    
    Adds 5 random users to the database.
    In order to log in to the dashboard, an admin user and role has been added.
    Count of each schema seeded has been added at end of file.
    
    Credentials for admin:
    ----
      + name:     "localwelcome-admin",
      + email:    "admin@localwelcome.com",
      + password: "localwelcome",

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,31 @@ Chapter.create(name: 'Manchester', description: Faker::Lorem.paragraph)
   i -= 2
   Event.create(chapter: london, title: Faker::Lorem.sentence, starts_at: i.months.from_now, ends_at: i.months.from_now + 2.hours)
 end
+
+5.times do
+  user = User.new(
+    name:     Faker::Name.first_name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10),
+  )
+  user.save!
+end
+
+admin = User.new(
+  name:     "localwelcome-admin",
+  email:    "admin@localwelcome.com",
+  password: "localwelcome",
+)
+admin.save!
+
+admin_role = Role.new(
+  user: admin,
+  chapter: nil,
+  role: "admin"
+)
+admin_role.save!
+
+puts "Chapters count: #{Chapter.count}"
+puts "Events count: #{Event.count}"
+puts "Users count: #{User.count}"
+puts "Roles count: #{Role.count}"
