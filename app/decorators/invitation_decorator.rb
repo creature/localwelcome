5af899a78fec708b36f3ae424058b0bcab425ce9
Alex Pounds <alex+git@alexpounds.com>
2015-12-10 11:02:22 -0500

    Fix typo/copy bug.

--- app/decorators/invitation_decorator.rb
+++ app/decorators/invitation_decorator.rb
@@ -2,8 +2,8 @@ class InvitationDecorator < Draper::Decorator
   delegate_all
 
   def friendly_status
-    return "object requested" if object.requested?
-    return "object sent, no response yet" if  object.sent?
+    return "Invitation requested" if object.requested?
+    return "Invitation sent, no response yet" if  object.sent?
     return "Not attending" if object.declined?
     return "Attending" if object.accepted?
     return "Invitation cancelled" if object.cancelled?
