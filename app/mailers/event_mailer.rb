f8a835b2ccf5cb12e077869f04dea28fc74b2129
Alex Pounds <alex+git@alexpounds.com>
2015-12-10 17:24:58 -0500

    Add a reminder mailer so we can remind people (later) about the events that are coming up.

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -7,4 +7,22 @@ class EventMailer < ApplicationMailer
     @event = event
     mail to: user.email, subject: "Local Welcome #{event.chapter.name} - our upcoming event '#{event.title}'"
   end
+
+  # Send a user a reminder about an event to a user.
+  def first_reminder(user, invite)
+    reminder_helper(user, invite, "First reminder about Local Welcome #{invite.event.chapter.name}'s #{invite.event.title}")
+  end
+
+  def last_reminder(user, invite)
+    reminder_helper(user, invite, "Final reminder about Local Welcome #{invite.event.chapter.name}'s #{invite.event.title}")
+  end
+
+  protected
+
+  def reminder_helper(user, invite, subject)
+    @user = user
+    @invite = invite
+    @event = invite.event
+    mail to: user.email, subject: subject, template_name: "reminder"
+  end
 end
--- spec/mailers/previews/event_mailer_preview.rb
+++ spec/mailers/previews/event_mailer_preview.rb
@@ -2,4 +2,12 @@ class EventMailerPreview < ActionMailer::Preview
   def announcement
     EventMailer.announcement(User.first, Event.first)
   end
+
+  def first_reminder
+    EventMailer.first_reminder(User.first, Invitation.first)
+  end
+
+  def last_reminder
+    EventMailer.last_reminder(User.first, Invitation.first)
+  end
 end
