ed038e58ef3674f32204e17a57e5aaaec6b3249b
Alex Pounds <alex+git@alexpounds.com>
2015-12-15 14:41:55 -0500

    Remove some un-needed comments from the test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,14 +16,9 @@ Rails.application.configure do
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
 
-  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
-  # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
-
-  # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
   # Tell Action Mailer not to deliver emails to the real world.
@@ -32,10 +27,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'localwelcome.dev' }
 
-  # Randomize the order test cases are executed.
   config.active_support.test_order = :random
-
-  # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
