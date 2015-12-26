0cdbdb4376a005458400c4d9c4dc314634014ad3
Alex Pounds <alex+git@alexpounds.com>
2015-12-26 17:05:56 -0500

    Make sure only organisers/admins get a handy link to manage this event

--- spec/features/browsing_events_spec.rb
+++ spec/features/browsing_events_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 feature "Browsing events" do
   let (:event) { FactoryGirl.create(:event) }
   let (:unpublished_event) { FactoryGirl.create(:unpublished_event) }
+  let (:user) { FactoryGirl.create(:user) }
+  let (:admin) { FactoryGirl.create(:admin) }
+  let (:organiser) { FactoryGirl.create(:chapter_organiser, chapter: event.chapter) }
+  let (:other_organiser) { FactoryGirl.create(:chapter_organiser) }
 
   scenario "An anonymous user can view a published event" do
     visit chapter_event_path(event.chapter, event)
@@ -15,5 +19,40 @@ feature "Browsing events" do
     visit chapter_event_path(unpublished_event.chapter, unpublished_event)
 
     expect(current_path).to eq root_path
+  end
+
+  context "The 'manage this event' button" do
+    scenario "An anonymous user doesn't see a manage button" do
+      visit chapter_event_path(event.chapter, event)
+
+      expect(page).not_to have_link "Manage this event"
+    end
+
+    scenario "A logged-in user doesn't see a manage button" do
+      manage_helper(user, event, false)
+    end
+
+    scenario "A chapter organiser for a different chapter doesn't see a manage button" do
+      manage_helper(other_organiser, event, false)
+    end
+
+    scenario "A chapter organiser for this chapter sees a manage button" do
+      manage_helper(organiser, event, true)
+    end
+
+    scenario "An admin sees a chapter button" do
+      manage_helper(admin, event, true)
+    end
+  end
+
+  def manage_helper(u, event, should_see_link)
+    login_as(u)
+    visit chapter_event_path(event.chapter, event)
+
+    if should_see_link
+      expect(page).to have_link "Manage this event"
+    else
+      expect(page).not_to have_link "Manage this event"
+    end
   end
 end
