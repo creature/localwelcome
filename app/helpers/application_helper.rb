4f564765bf63285e48b788ecdab1e30192c4c198
Alex Pounds <alex+git@alexpounds.com>
2016-01-17 18:54:47 -0500

    Render banners in a helper method, to DRY up the code in this template.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def render_admin_prompt(heading, link_text, link_target, explanation)
+    content_tag :div, class: "banner bg-warning text-warning" do
+      content_tag :div, class: "container" do
+        content_tag(:h1, "#{heading} &ndash; ".html_safe + link_to("#{link_text} &raquo;".html_safe, link_target)) +
+        content_tag(:p, explanation)
+      end
+    end
+  end
 end
