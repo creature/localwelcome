f135bea0a1fabb66c8f88b9113805c71373e3171
Alex Pounds <alex+git@alexpounds.com>
2016-04-03 19:00:19 -0400

    Support the rendering of Markdown fields in the event decorator, as well as some text representations of those Markdown fields.

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -1,12 +1,22 @@
 class EventDecorator < Draper::Decorator
+  include ApplicationHelper # This works around some Draper weirdness when using proxied helpers in mailers. Don't call @event.render_markdown.
   delegate_all
 
   def friendly_description
-    field_helper(:description)
+    field_helper(:description, true)
+  end
+
+  def friendly_text_description
+    field_helper_as_text(:description)
   end
 
   def email_info
-    field_helper(:email_info)
+    field_helper(:email_info, true)
+  end
+
+  # Email info is optional; render the Markdown, if we have it.
+  def optional_email_info_as_text
+    render_markdown_as_text(object.email_info) unless object.email_info.blank?
   end
 
   def venue_name
@@ -18,7 +28,16 @@ class EventDecorator < Draper::Decorator
   end
 
   def venue_info
-    field_helper(:venue_info)
+    field_helper(:venue_info, true)
+  end
+
+  # `venue_info` will render a "No venue info yet" tag if it's missing; this one will just output nothing.
+  def optional_venue_info
+    render_markdown(object.venue_info) unless object.venue_info.blank?
+  end
+
+  def optional_venue_info_as_text
+    render_markdown_as_text(object.venue_info) unless object.venue_info.blank?
   end
 
   def friendly_venue_name
@@ -44,9 +63,13 @@ class EventDecorator < Draper::Decorator
 
   protected
 
-  def field_helper(field)
+  # Display the field if it's not blank, otherwise render <span class="system message">No [fieldname] yet</span>.
+  # Optionally supports Markdown.
+  def field_helper(field, markdown=false)
     if object.public_send(field).blank?
       h.content_tag :span, "No #{field.to_s.humanize.downcase} yet", class: "system message"
+    elsif markdown
+      render_markdown object.public_send(field)
     else
       object.public_send(field)
     end
