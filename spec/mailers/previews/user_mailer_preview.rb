8225a56c59e608fe213f8432992dd371e0987f25
Alex Pounds <alex+git@alexpounds.com>
2016-04-03 19:02:25 -0400

    Support Markdown rendering in events - on the admin view, the user view, and in event mailers; add a mailer preview to easily verify that.

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,12 @@
 class UserMailerPreview < ActionMailer::Preview
+  def invite_with_markdown_info
+    invite = Invitation.first
+    invite.event.email_info = "I'm **some** [Markdown](https://github.com/) email info."
+    invite.event.venue_info = "We'll be meeting **downstairs** this time."
+    invite.event.description = "We're holding this event on _Saturday_ this month, unlike last time."
+    UserMailer.invite(invite)
+  end
+
   def invite_with_no_extra_info
     invite = Invitation.first
     %i{email_info venue_name venue_postcode venue_info}.each { |s| invite.event.public_send("#{s}=", nil) }
@@ -19,6 +27,15 @@ class UserMailerPreview < ActionMailer::Preview
     invite.event.venue_name = "Starbucks"
     invite.event.venue_postcode = "SW1E 6RD"
     invite.event.venue_info = "Upstairs, on a table with a 'Local Welcome' sign."
+    UserMailer.invite(invite)
+  end
+
+  def invite_with_some_venue_info
+    invite = Invitation.first
+    invite.event.email_info = nil
+    invite.event.venue_name = "Starbucks"
+    invite.event.venue_postcode = "SW1E 6RD"
+    invite.event.venue_info = nil
     UserMailer.invite(invite)
   end
 
